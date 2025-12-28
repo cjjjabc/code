52a19e60425fae823ebc36f2885486d184033815
cjjabc <3356783426@qq.com>
2025-12-28 22:06:22 +0800

    main.c

--- main.c
+++ main.c
@@ -1,4 +1,13 @@
 #include<stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "auth.h"
+#include "utils.h"
+#include "club.h"
+#include "finance.h"
+#include "activity.h"
+
 int main(){
+
   return 0;
 }
